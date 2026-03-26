9620fe9fdd0f40eb1fe44ac69f819861f173c6a9
W3C CT <web3currency.info@gmail.com>
2026-03-26 07:18:48 +0100

    Add Speed Insights initialization

--- index.tsx
+++ index.tsx
@@ -1,16 +1,7 @@
+import SpeedInsights from 'speed-insights';
+
+SpeedInsights.initialize();
 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ...rest of your code...
